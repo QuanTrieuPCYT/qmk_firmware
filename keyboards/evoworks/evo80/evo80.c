0cf97bb6d9c8b212658580dbcb07bf2b824a7344
QuanTrieuPCYT <admin@qtpc.tech>
2025-12-25 23:53:23 +0700

    remove non-needed function

--- keyboards/evoworks/evo80/evo80.c
+++ keyboards/evoworks/evo80/evo80.c
@@ -157,22 +157,6 @@ static inline bool kb_get_caps_lock_state(void) {
     return (Keyboard_Status.System_Led_Status & (1 << 1));
 }
 
-static inline uint8_t get_led_index_for_keycode(uint16_t target_keycode, uint8_t layer) {
-    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-        for (uint8_t col = 0; col < MATRIX_COLS; col++) {
-            uint8_t led_index = g_led_config.matrix_co[row][col];
-            if (led_index == NO_LED) {
-                continue; 
-            }
-            keypos_t pos = {.row = row, .col = col};
-            if (keymap_key_to_keycode(layer, pos) == target_keycode) {
-                return led_index;
-            }
-        }
-    }
-    return NO_LED;
-}
-
 static void Set_Factory_Defaults(void)
 {
     Keyboard_Info.Batt_Number     = 50;
